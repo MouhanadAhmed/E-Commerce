13a3cb4e7403d293ccb9202e866116e2036d4d0f
Mouhanad Ahmed <mouhanad.aboelmakarem@gmail.com>
2023-06-29 18:13:19 +0300

    compressed images and fixed long dir

--- src/Components/MainPage/MainSlider/MainSlider.jsx
+++ src/Components/MainPage/MainSlider/MainSlider.jsx
@@ -19,48 +19,48 @@ export default function MainSlider() {
     <div className="container px-0 mx-auto py-1 mb-5 ">
     <Slider {...settings} className='w-100'>
     <div>
-        <img style={{'height':'600px'}} src={require('../../../Assets/Images/fashionable-model-stylish-hat-red-coat-boots-posing-white-wall-studio.jpg')} className='w-100 rounded-2' alt="slider-1" />
+        <img style={{'height':'600px'}} src={require('../../../Assets/Images/fashionable-model.jpg')} className='w-100 rounded-2' alt="slider-1" />
       </div>
       <div>
-      <img style={{'height':'600px'}} src={require('../../../Assets/Images/high-fashion-portrait-young-elegant-blonde-woman-black-wool-hat-wearing-oversize-white-fringe-poncho-with-long-grey-dress.jpg')} className='w-100 rounded-2' alt="slider-2" />
+      <img style={{'height':'600px'}} src={require('../../../Assets/Images/high-fashion.jpg')} className='w-100 rounded-2' alt="slider-2" />
       </div>
       <div>
-      <img style={{'height':'600px'}} src={require('../../../Assets/Images/woman-model-business-suit-wearing-hat.jpg')} className='w-100 rounded-2' alt="slider-3" />
+      <img style={{'height':'600px'}} src={require('../../../Assets/Images/woman-model.jpg')} className='w-100 rounded-2' alt="slider-3" />
 
       </div>
       <div>
-      <img style={{'height':'600px'}} src={require('../../../Assets/Images/beautiful-men-fashion-wooden-background.jpg')} className='w-100 rounded-2' alt="grocery-banner" />
+      <img style={{'height':'600px'}} src={require('../../../Assets/Images/beautiful-men.jpg')} className='w-100 rounded-2' alt="grocery-banner" />
 
       </div>
       <div>
-      <img style={{'height':'600px'}} src={require('../../../Assets/Images/portrait-handsome-smiling-young-man-model-wearing-casual-summer-pink-clothes-fashion-stylish-man-posing-round-sunglasses.jpg')} className='w-100 rounded-2' alt="grocery-banner-2" />
+      <img style={{'height':'600px'}} src={require('../../../Assets/Images/portrait-handsome.jpg')} className='w-100 rounded-2' alt="grocery-banner-2" />
       </div>
       <div>
-      <img style={{'height':'600px'}} src={require('../../../Assets/Images/set-with-fashionable-women-s-clothing-jeans-sweater.jpg')} className='w-100 rounded-2' alt="slider-2" />
+      <img style={{'height':'600px'}} src={require('../../../Assets/Images/portrait-handsome-sm.jpg')} className='w-100 rounded-2' alt="slider-2" />
 
       </div>
       <div>
-      <img style={{'height':'600px'}} src={require('../../../Assets/Images/portrait-handsome-fashion-stylish-hipster-businessman-model-dressed-elegant-brown-suit-sitting-near-dark.jpg')} className='w-100 rounded-2' alt="slider-2" />
+      <img style={{'height':'600px'}} src={require('../../../Assets/Images/wonderful-womanl.jpg')} className='w-100 rounded-2' alt="slider-2" />
 
       </div>
       <div>
-      <img style={{'height':'600px'}} src={require('../../../Assets/Images/stylish-attractive-young-woman-casual-white-costume-blue-jacket-holding-luxury-purse.jpg')} className='w-100 rounded-2' alt="slider-2" />
+      <img style={{'height':'600px'}} src={require('../../../Assets/Images/stylish-attractive.jpg')} className='w-100 rounded-2' alt="slider-2" />
 
       </div>
       <div>
-      <img style={{'height':'600px'}} src={require('../../../Assets/Images/elegant-woman-black-hat-evening-dress-leather-jacket-posing-white.jpg')} className='w-100 rounded-2' alt="slider-2" />
+      <img style={{'height':'600px'}} src={require('../../../Assets/Images/elegant-woman.jpg')} className='w-100 rounded-2' alt="slider-2" />
 
       </div>
       <div>
-      <img style={{'height':'600px'}} src={require('../../../Assets/Images/gorgeous-white-girl-with-long-wavy-hair-chilling-autumn-day-outdoor-portrait-interested-ginger-female-model-with-cup-coffee.jpg')} className='w-100 rounded-2' alt="slider-2" />
+      <img style={{'height':'600px'}} src={require('../../../Assets/Images/fashionable-model.jpg')} className='w-100 rounded-2' alt="slider-2" />
 
       </div>
       <div>
-      <img style={{'height':'600px'}} src={require('../../../Assets/Images/full-length-view-wonderful-woman-dancing-turquoise-wall.jpg')} className='w-100 rounded-2' alt="slider-2" />
+      <img style={{'height':'600px'}} src={require('../../../Assets/Images/high-fashion.jpg')} className='w-100 rounded-2' alt="slider-2" />
 
       </div>
       <div>
-      <img style={{'height':'600px'}} src={require('../../../Assets/Images/pretty-red-head-woman-yellow-dress-posing-yellow-summer-mood.jpg')} className='w-100 rounded-2' alt="slider-2" />
+      <img style={{'height':'600px'}} src={require('../../../Assets/Images/pretty-red.jpg')} className='w-100 rounded-2' alt="slider-2" />
 
       </div>
     </Slider>
